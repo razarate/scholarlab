419b703d62ee62f8e6635f4652e12f0c327aef5b
RaZaMo <horco1@gmail.com>
2020-11-09 23:06:07 -0600

    Adding administrator view

--- com_scholarlab/admin/scholarlab.php
+++ com_scholarlab/admin/scholarlab.php
@@ -1,1 +1,16 @@
-Hello World Administration
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+JHtml::_('behavior.tabstate');
+
+
+$controller = JControllerLegacy::getInstance('scholarlab');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
