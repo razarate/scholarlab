e2c7ea06afb4dd13ab0b88e6c3bbc2f3a0237a06
Piensocial.com <admin@piensocial.com>
2021-04-01 10:44:29 -0600

    A model to manage everything about sensors measurements

--- com_scholarlab/site/models/sensor.php
+++ com_scholarlab/site/models/sensor.php
@@ -28,20 +28,21 @@ class ScholarlabModelSensor extends JModelList {
 		return JTable::getInstance($type, $prefix, $config);
 	}
 
-	public function getSensorList() {
+	public function getSensorList($sensorid = NULL) {
 		// Get a db connection.
 		$db = JFactory::getDbo();
 
 		// Create a new query object.
 		$query = $db->getQuery(true);
 
-		// Select all records from the user profile table where key begins with "custom.".
-		// Order it by the ordering field.
 		$query
 			->select('*')
 			->from($db->quoteName('#__scholarlab_sensor_details'))
-			->where($db->quoteName('published') . " = 1")
-			/*->group($db->quoteName('sensor_type'))*/;
+			->where($db->quoteName('published') . " = 1");
+
+		if ($sensorid) {
+			$query->where($db->quoteName('id') . " = " . $sensorid);
+		}
 
 		// Reset the query using our newly populated query object.
 		$db->setQuery($query);
@@ -52,6 +53,7 @@ class ScholarlabModelSensor extends JModelList {
 		return $results;
 
 	}
+
 
 	/**
 	 * Insert sensor data
@@ -79,7 +81,208 @@ class ScholarlabModelSensor extends JModelList {
 		return FALSE;
 	}
 
-	protected function distinctRecords($sensorType = NULL, $sensorId = NULL, $fromDate = NULL, $toDate = NULL) {
+	/**
+	 * Get sensor data
+	 *
+	 * @param   Text 	$sensor_type
+	 * @param 	Text 	$sensor_id
+	 * @param 	json 	$data
+	 * @return  true / false
+	 */
+	public function getSensorReadings($sensorid = NULL, $fromDate = NULL, $toDate = NULL) {
+
+		if (is_null($sensorid)) {
+			return false;
+		}
+
+		$records = $this->distinctRecords($sensorid, $fromDate, $toDate);
+		$days = count($records);
+
+		if ($days == 0) {
+			return false;
+		}
+
+		if ($days < 4) {
+			$timeframe = 'hour';
+		} else {
+			$timeframe = 'day';
+		}
+
+		// Check sensor Type
+		$sensorInfo = self::getSensorList($sensorid);
+		if ($sensorInfo[0]['sensor_type'] == 'bme280') {
+			$sensorReadings = self::bme280ChartData($records, $sensorid, $timeframe);
+		}
+
+		if ($sensorInfo[0]['sensor_type'] == 'ds18b20') {
+			$sensorReadings = self::ds18b20ChartData($records, $sensorid, $timeframe);
+		}
+
+		return $sensorReadings; 
+	}
+
+
+	protected function bme280ChartData($records = array(), $sensorid = NULL, $timeframe = 'day') {
+		// Get a db connection.
+		$db = JFactory::getDbo();
+
+		// Create a new query object.
+		$query = $db->getQuery(true);
+
+		$i = 0;
+		if ($timeframe == 'hour') {
+
+			$records = self::timeFrimeHours($records, $sensorid);
+
+			while ($i < count($records)) {
+				// Create a new query object.
+				$query = $db->getQuery(true);
+
+				$query
+					->select("AVG(JSON_EXTRACT(data, '$.Temp')), AVG(JSON_EXTRACT(data, '$.Humidity')),
+							AVG(JSON_EXTRACT(data, '$.Pressure')), AVG(JSON_EXTRACT(data, '$.Alt'))")
+					->from($db->quoteName('#__scholarlab_sensor_measurement'))
+					->where($db->quoteName('sensorid') . ' = ' . $sensorid)
+					->where($db->quoteName('created') . ' BETWEEN ' . $db->quote($records[$i][1] . ' ' . $records[$i][0] .':00:00') . ' AND ' . $db->quote($records[$i][1] . ' ' . $records[$i][0] .':59:59'));
+
+				// Reset the query using our newly populated query object.
+				$db->setQuery($query);
+
+				// Packing results to return data
+				$rawData = $db->loadRow();
+			    $tempData[$i] = $rawData[0];
+			    $humidityData[$i] = $rawData[1];
+
+			    $d = new DateTime($records[$i][1] . ' ' . $records[$i][0] . ':00:00');
+			    $dateData[$i] = "'" . date_format($d, 'd-m-Y H:i') . "'";
+
+			    $i++;
+			}
+		} elseif ($timeframe == 'day') {
+
+			while ($i < count($records)) {
+			// Create a new query object.
+			$query = $db->getQuery(true);
+
+			$query
+				->select("AVG(JSON_EXTRACT(data, '$.Temp')), AVG(JSON_EXTRACT(data, '$.Humidity')),
+							AVG(JSON_EXTRACT(data, '$.Pressure')), AVG(JSON_EXTRACT(data, '$.Alt'))")
+				->from($db->quoteName('#__scholarlab_sensor_measurement'))
+				->where($db->quoteName('sensorid') . ' = ' . $sensorid)
+				->where($db->quoteName('created') . ' BETWEEN ' . $db->quote($records[$i] . ' 00:00:00') . ' AND ' . $db->quote($records[$i] . ' 23:59:59'));
+
+			// Reset the query using our newly populated query object.
+			$db->setQuery($query);
+
+			// Packing results to return data
+			$rawData = $db->loadRow();
+			$tempData[$i] = $rawData[0];
+			$humidityData[$i] = $rawData[1];
+
+			$d = new DateTime($records[$i] . ' 00:00:00');
+			$dateData[$i] = "'" . date_format($d, 'd-m-Y') . "'";
+
+			$i++;
+			}
+		}
+
+		// Packing arrays to return data
+        $bme280GraphData['temp'] = array_reverse($tempData);
+        $bme280GraphData['humidity'] = array_reverse($humidityData);
+        $bme280GraphData['date'] = array_reverse($dateData);
+
+        return $bme280GraphData; 
+	}
+
+
+	protected function ds18b20ChartData($records = array(), $sensorid = NULL, $timeframe = 'day') {
+		// Get a db connection.
+		$db = JFactory::getDbo();
+
+		// Create a new query object.
+		$query = $db->getQuery(true);
+
+		$i = 0;
+		if ($timeframe == 'hour') {
+
+			$records = self::timeFrimeHours($records, $sensorid);
+
+			while ($i < count($records)) {
+				// Create a new query object.
+				$query = $db->getQuery(true);
+
+				$query
+					->select("AVG(JSON_EXTRACT(data, '$.Temp'))")
+					->from($db->quoteName('#__scholarlab_sensor_measurement'))
+					->where($db->quoteName('sensorid') . ' = ' . $sensorid)
+					->where($db->quoteName('created') . ' BETWEEN ' . $db->quote($records[$i][1] . ' ' . $records[$i][0] .':00:00') . ' AND ' . $db->quote($records[$i][1] . ' ' . $records[$i][0] .':59:59'));
+
+				// Reset the query using our newly populated query object.
+				$db->setQuery($query);
+
+				// Packing results to return data
+				$rawData = $db->loadRow();
+			    $tempData[$i] = $rawData[0];
+
+			    $d = new DateTime($records[$i][1] . ' ' . $records[$i][0] . ':00:00');
+			    $dateData[$i] = "'" . date_format($d, 'd-m-Y H:i') . "'";
+
+			    $i++;
+			}
+		} elseif ($timeframe == 'day') {
+
+			while ($i < count($records)) {
+			// Create a new query object.
+			$query = $db->getQuery(true);
+
+			$query
+				->select("AVG(JSON_EXTRACT(data, '$.Temp'))")
+				->from($db->quoteName('#__scholarlab_sensor_measurement'))
+				->where($db->quoteName('sensorid') . ' = ' . $sensorid)
+				->where($db->quoteName('created') . ' BETWEEN ' . $db->quote($records[$i] . ' 00:00:00') . ' AND ' . $db->quote($records[$i] . ' 23:59:59'));
+
+			// Reset the query using our newly populated query object.
+			$db->setQuery($query);
+
+			// Packing results to return data
+			$rawData = $db->loadRow();
+			$tempData[$i] = $rawData[0];
+
+			$d = new DateTime($records[$i] . ' 00:00:00');
+			$dateData[$i] = "'" . date_format($d, 'd-m-Y') . "'";
+
+			$i++;
+			}
+		}
+
+		// Packing arrays to return data
+        $ds18b20ChartData['temp'] = array_reverse($tempData);
+        $ds18b20ChartData['date'] = array_reverse($dateData);
+
+        return $ds18b20ChartData; 
+	}
+
+	protected function timeFrimeHours($records = array(), $sensorid = NULL){
+		// Get a db connection.
+		$db = JFactory::getDbo();
+		// Create a new query object.
+		$query = $db->getQuery(true);
+
+		$query
+			->select($query->hour($query->quoteName('created')))
+			->select("DATE(" . $db->quoteName('created') . ")")
+			->from($db->quoteName('#__scholarlab_sensor_measurement'))
+			->where($db->quoteName('sensorid') . ' = ' . $sensorid)
+			->where($db->quoteName('created') . ' BETWEEN ' . $db->quote(end($records) . ' 00:00:00') . ' AND ' . $db->quote($records[0] . ' 23:59:59'))
+			->group($query->day($query->quoteName('created')))
+			->group($query->hour($query->quoteName('created')))
+			->order(array($query->day($query->quoteName('created')) . ' ASC', $query->hour($query->quoteName('created')) . ' ASC'));
+		$db->setQuery($query);
+		
+		return $db->loadRowList();
+	}
+
+	protected function distinctRecords($sensorid = NULL, $fromDate = NULL, $toDate = NULL) {
 		// Get a db connection.
 		$db = JFactory::getDbo();
 
@@ -91,16 +294,11 @@ class ScholarlabModelSensor extends JModelList {
 		$query
 			->select("DISTINCT DATE(" . $db->quoteName('created') . ")")
 			->from($db->quoteName('#__scholarlab_sensor_measurement'))
-			->where($db->quoteName('sensor_type') . ' LIKE ' . $db->quote($sensorType))
+			->where($db->quoteName('sensorid') . ' = ' . $sensorid)
 			->order($query->quoteName('created') . ' DESC');
-		
-		// Select specific sensor ID
-		if (!is_null($sensorId)) {
-			$query->where($db->quoteName('sensor_id') . ' LIKE ' . $db->quote($sensorId));
-		}
 
 		// Limit query
-		if (!is_null($fromDate) AND !is_null($toDate)) {
+		if (!is_null($fromDate) || !is_null($toDate)) {
 			$query->where($db->quoteName('created') . ' BETWEEN ' . $db->quote($fromDate . ' 00:00:00') . ' AND ' . $db->quote($toDate . ' 23:59:59'));
 		} else {
 			$query->setLimit('24');
