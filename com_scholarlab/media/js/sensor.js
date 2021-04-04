d5b8f8d9c2aba4a01c564f4efbccb2b92688fd16
Piensocial.com <admin@piensocial.com>
2021-04-04 18:29:43 -0500

    partial views

--- com_scholarlab/media/js/sensor.js
+++ com_scholarlab/media/js/sensor.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
 
-    setInterval(isRoomReady, 5000);
+    setInterval(checkSensor, 3000);
 
 });
 
-function isRoomReady() {
+function checkSensor() {
 
     //var token = jQuery("#token").attr("name");
 
@@ -15,14 +15,20 @@ function isRoomReady() {
                 const resultObject = JSON.parse(result.data);
                 //console.log('Ajax response: ' + result.data);
                 //console.log('0x76: ' + resultObject['0x76']['Temp']);
-                document.getElementById("bme280Temp").innerHTML = parseFloat(resultObject['0x76']['Temp']).toFixed(2);
-                document.getElementById("bme280Humidity").innerHTML = parseFloat(resultObject['0x76']['Humidity']).toFixed(2);
-                document.getElementById("bme280Alt").innerHTML = parseFloat(resultObject['0x76']['Alt']).toFixed(2);
-                document.getElementById("ds18b20_1").innerHTML = parseFloat(resultObject['28-01191ed83f34']['Temp']).toFixed(2);
-                document.getElementById("ds18b20_2").innerHTML = parseFloat(resultObject['28-01191ecb8132']['Temp']).toFixed(2);
-                document.getElementById("ds18b20_3").innerHTML = parseFloat(resultObject['28-020a91772089']['Temp']).toFixed(2);
-                
-                
+                if (document.getElementById("bme280Temp")) {
+                    document.getElementById("bme280Temp").innerHTML = parseFloat(resultObject['0x76']['Temp']).toFixed(2);
+                    document.getElementById("bme280Humidity").innerHTML = parseFloat(resultObject['0x76']['Humidity']).toFixed(2);
+                    //document.getElementById("bme280Alt").innerHTML = parseFloat(resultObject['0x76']['Alt']).toFixed(2);
+                    document.getElementById("bme280Pressure").innerHTML = parseFloat(resultObject['0x76']['Pressure']).toFixed(2);
+                }
+                if (document.getElementById("ds18b20_1")) {
+                    document.getElementById("ds18b20_1").innerHTML = parseFloat(resultObject['28-01191ed83f34']['Temp']).toFixed(2);
+                    document.getElementById("ds18b20_2").innerHTML = parseFloat(resultObject['28-01191ecb8132']['Temp']).toFixed(2);
+                }
+                if (document.getElementById("ds18b20_3")) {
+                    document.getElementById("ds18b20_3").innerHTML = parseFloat(resultObject['28-020a91772089']['Temp']).toFixed(2);
+                }
+
                 /*
                 if (resultObject.status == 'opened') {
                     submitForm();
